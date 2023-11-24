279b2e00493ad09949bb056ca6f0ac084c8e1816
jbenavides <jhonattangbc@udenar.edu.co>
2023-11-24 09:21:42 -0500

    pantalla login - cambio 1

--- app/src/main/java/com/example/uberapp/MainActivity.kt
+++ app/src/main/java/com/example/uberapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.uberapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.uberapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener{ goToRegister() }
+    }
+
+    private fun goToRegister() {
+        val i = Intent(this, RegisterActivity::class.java)
+        startActivity(i)
     }
 }
