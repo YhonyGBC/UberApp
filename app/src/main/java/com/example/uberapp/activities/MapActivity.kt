ae9b9fa1edea5d24de2feeac21178a945529e079
jbenavides <jhonattangbc@udenar.edu.co>
2023-12-13 11:17:10 -0500

    commit ubicación, zoom, y girar mapa

--- app/src/main/java/com/example/uberapp/activities/MapActivity.kt
+++ app/src/main/java/com/example/uberapp/activities/MapActivity.kt
@@ -1,13 +1,50 @@
 package com.example.uberapp.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.easywaylocation.EasyWayLocation
+import com.example.easywaylocation.Listener
 import com.example.uberapp.R
 import com.example.uberapp.databinding.ActivityMapBinding
+import com.example.uberapp.providers.AuthProvider
+import com.example.uberapp.providers.GeoProvider
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
 
     private lateinit var binding: ActivityMapBinding
+    private var googleMap: GoogleMap? = null
+    private var easyWayLocation: EasyWayLocation? = null
+    // latitud y longitud en la que nos encontramos actualmente
+    private var myLocationLatLng: LatLng? = null
+    private val geoProvider = GeoProvider()
+    private val authProvider = AuthProvider()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +52,107 @@ class MapActivity : AppCompatActivity() {
         binding = ActivityMapBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        /* características para instanciar la localización, pide la actualización de ubicación en el mínimo intervalo,
+           con una alta precisión en la ubicación y que establece el cambio u actualización en 1 metro
+        */
+        val locationRequest = LocationRequest.create().apply {
+            interval = 0
+            fastestInterval = 0
+            priority = Priority.PRIORITY_HIGH_ACCURACY
+            smallestDisplacement = 1f
+        }
+
+        easyWayLocation = EasyWayLocation(this, locationRequest, false, false, this)
+
+        // se solicita los permisos para acceder a la ubicación
+        locationPermissions.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ))
+
     }
+
+    // dar permisos para acceder a la localización
+    val locationPermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permission ->
+        // la versión de android es mayor a la versión de Android Nougat (7.0, API level 24).
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            when {
+                permission.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    Log.d("LOCALIZACIÓN", "Permiso concedido")
+                    easyWayLocation?.startLocation()
+                }
+                permission.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    Log.d("LOCALIZACIÓN", "Permiso concedido con limitación")
+                    easyWayLocation?.startLocation()
+                }
+                else -> {
+                    Log.d("LOCALIZACIÓN", "Permiso no concedido")
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume() // al abrir la pantalla actual
+    }
+
+    override fun onDestroy() { // se ejecuta cuando se cierra la aplicación o se pasa a otra activity
+        super.onDestroy()
+        easyWayLocation?.endUpdates()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        googleMap?.uiSettings?.isZoomControlsEnabled = true // se visualiza los controles del zoom
+        // easyWayLocation?.startLocation()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
+        // marcador de ubicación
+        googleMap?.isMyLocationEnabled = true
+        // cargar nuevo estilo del mapa, tomado de https://mapstyle.withgoogle.com/
+        try {
+            val success = googleMap?.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(this, R.raw.style)
+            )
+            if (!success!!) {
+                Log.d("MAPAS", "No se pudo econtrar el estilo")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.d("MAPAS", "Error: ${e.toString()}")
+        }
+    }
+
+    override fun locationOn() {
+
+    }
+
+    override fun currentLocation(location: Location) { // actualiza la posición en tiempo real
+        myLocationLatLng = LatLng(location.latitude, location.longitude) // lat y long actual
+
+        googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition( // permite hacer zoom y girar mapa
+            CameraPosition.builder().target(myLocationLatLng!!).zoom(17f).build()
+        ))
+    }
+
+    override fun locationCancelled() {
+
+    }
+
+
 }
